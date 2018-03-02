408cbe28ae3998f3e4334b8a66892e0b8ae739a6
jlkruguer <jlkruguer10@gmail.com>
2018-03-01 23:14:06 -0500

    Update whitenData.m
    update

--- HW2/code/problem3code/whitenData.m
+++ HW2/code/problem3code/whitenData.m
@@ -8,12 +8,11 @@ mean_subtracted = data - repmat(avg, size(data,1), 1);
 
 covA = covariance_matrix(mean_subtracted, data);
 
-
 [eigen_vectors, eigen_values] = eig(covA);
 
 
 % 3) Scale pc with corresponding eigenvalue
-whitened_data = diag(1./sqrt(diag(eigen_values))) * eigen_vectors' * mean_subtracted;
-
+scalar_multiplier = transpose(diag(eigen_values).^(0.5));
+scaled_eigen_vectors = eigen_vectors .* scalar_multiplier;
+whitened_data = mean_subtracted * scaled_eigen_vectors; 
 end
-
